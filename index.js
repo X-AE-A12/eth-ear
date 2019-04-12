e0085f0f19e5901dcf05c66626f5564148715b31
yanesh <yaneshcode@gmail.com>
2019-04-11 20:06:31 -0400

    index

---
+++ index.js
@@ -0,0 +1,24 @@
+const ethers = require('ethers');
+
+function eventListener(eventName, contractAddress, abi, network) {
+  let provider = new ethers.getDefaultProvider(network);
+  let contract = new ethers.Contract(contractAddress, abi, provider);
+
+
+  (async function() {
+    let transferEvent = new Promise((resolve, reject) => {
+      contract.on(eventName, (from, to, amount, event) => {
+        console.log("From  : " + from);
+        console.log("To    : " + to);
+        console.log("Amount: " + amount);
+        console.log("\n");
+
+        resolve({
+          from: from,
+          to: to,
+          amount: amount
+        });
+      });
+    });
+  })();
+}
